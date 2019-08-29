584b67acc469f2496adedee856e68bd28a55f3b7
DMa <davidweibinma@Weibins-MacBook-Pro-2.local>
2019-08-29 13:50:19 -0400

    update

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
-const router = require("express").Router();
 const app = express();
 
 // Define middleware here
@@ -14,17 +13,7 @@ if (process.env.NODE_ENV === "production") {
 
 // Define API routes here
 // home page route (http://localhost:8080)
-router.get('/test', function(req, res) {
-  res.send('im the test page!');  
-});
 
-// about page route (http://localhost:8080/about)
-router.get('/about', function(req, res) {
-  res.send('im the about page!'); 
-});
-
-// apply the routes to our application
-app.use('/api', router);
 // Send every other request to the React app
 // Define any API routes before this runs
 app.get("*", (req, res) => {
